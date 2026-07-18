97076f601763dd9f9352b0b2a4018a379b0064b6
Vishal Dhariwal <dhariwalvishalvd@gmail.com>
2026-07-18 11:26:17 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -44,8 +44,15 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "*"
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
+        "https://machine-failure-prediction-alpha.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
